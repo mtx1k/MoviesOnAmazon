94fa2e5fd537644aaff69a363c319705409e6533
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-04-08 14:07:05 +0200

    added 2 movies and printed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Movie movie1 = new Movie();
+        movie1.title = "Der Herr der Ringe: Extended Edition Trilogie";
+        movie1.genre = MovieGenre.FANTASY;
+        movie1.price = 25;
+
+        Movie movie2 = new Movie();
+        movie2.title = "Allein zu Haus";
+        movie2.genre = MovieGenre.COMEDY;
+        movie2.price = 8;
+
+        printMovieDetails(movie1);
+        printMovieDetails(movie2);
     }
     public static void printMovieDetails(Movie movie) {
         System.out.println("Title: " + movie.title + "\n"
--- src/MovieGenre.java
+++ src/MovieGenre.java
@@ -2,5 +2,6 @@ public enum MovieGenre {
     DRAMA,
     COMEDY,
     ACTION,
+    FANTASY,
     DOCUMENTARY
 }
