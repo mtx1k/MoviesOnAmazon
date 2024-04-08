8ad3fb8f3f6bc577b6adeab2151a3836b9fb63c4
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-04-08 13:57:47 +0200

    created method printMovieDetails

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void printMovieDetails(Movie movie) {
+        System.out.println("Title: " + movie.title + "\n"
+        + "Genre: " + movie.genre + "\n"
+        + "Price: " + movie.price);
+    }
 }
